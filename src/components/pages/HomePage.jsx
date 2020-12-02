c8c54b31141ef6bd1f9bb3ae9839e44a003e4486
vedanthmohan20 <vedanth2805@gmail.com>
2020-12-02 15:19:47 +0530

    added logo to navbar

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import MyAppBar from "../MyAppBar";
 import MainContent from "../MainContent";
-
+let imageSource = "/public/logo.png";
 const HomePage = () => {
 	return (
 		<>
-			<MyAppBar title="PESLink" />
+			<MyAppBar title={<img src='https://github.com/Jacob3075/Social-Network-Client/blob/master/public/PESLink%20logo.png?raw=true' style={{width:200}}/>} />
 			<MainContent />
 		</>
 	);
