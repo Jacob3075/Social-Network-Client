806a897b7afdf5c8eba3cf784f4b8e2312e2abd6
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-12-05 12:30:09 +0530

    Separated TopicHeaderSection.jsx into a new component and cleaned up TopicPage.jsx.

--- src/components/pages/TopicPage.jsx
+++ src/components/pages/TopicPage.jsx
@@ -4,10 +4,9 @@ import MainContent from "../MainContent";
 import { userService } from "../../services/UserService";
 import { useHistory, useParams } from "react-router-dom";
 import { getPostsFromTopic } from "../../services/PostService";
-import { Typography } from "@material-ui/core";
 import { getEventsByTopic } from "../../services/EventService";
 import { getTopicById } from "../../services/TopicService";
-import TopicFollowButton from "../topics/TopicFollowButton";
+import TopicHeaderSection from "./TopicHeaderSection";
 
 const TopicPage = () => {
 	const history = useHistory();
@@ -16,27 +15,17 @@ const TopicPage = () => {
 	const [reload, setReload] = useState(false);
 	const [topicName, setTopicName] = useState("");
 	const [description, setDescription] = useState("");
-	const [isFollowedTopic, setIsFollowedTopic] = useState(false);
 
 	useEffect(() => {
-		setIsFollowedTopic(userService.getFollowedTopics().includes(topicId));
+		if (!userService.isLoggedIn()) history.push("/login");
+
+		getTopicById(topicId)
+			.then((topic) => {
+				setTopicName(topic.topicName);
+				setDescription(topic.description);
+			})
+			.catch((error) => console.log(error));
 	}, [reload]);
-
-	useEffect(() => {
-		if (!userService.isLoggedIn()) history.push("/login");
-
-		getTopicById(topicId)
-			.then((topic) => setTopicName(topic.topicName))
-			.catch((error) => console.log(error));
-	}, []);
-
-	useEffect(() => {
-		if (!userService.isLoggedIn()) history.push("/login");
-
-		getTopicById(topicId)
-			.then((topic) => setDescription(topic.description))
-			.catch((error) => console.log(error));
-		}, []);
 
 	const loadEvents = () => {
 		return getEventsByTopic(topicId)
@@ -63,17 +52,12 @@ const TopicPage = () => {
 	return (
 		<>
 			<MyAppBar title={""} setReload={setReload} reload={reload} />
-			<Typography variant="h4" color="textPrimary">
-				{topicName + "\xa0\xa0"}
-				<TopicFollowButton isFollowedTopic={isFollowedTopic} setReload={setReload} topicId={topicId}
-				                   topicName={topicName} />
-			</Typography>
-
-
-			<Typography variant="h6">
-				{description} 
-			</Typography>
-			
+			<TopicHeaderSection topicName={topicName}
+			                    topicId={topicId}
+			                    description={description}
+			                    setReload={setReload}
+			                    reload={reload}
+			/>
 			<MainContent
 				loadPosts={loadPosts}
 				loadEvents={loadEvents}
